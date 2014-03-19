162a305faa133fd535fc1395f0cd0129a8dc7825
Mathieu Gauthron <mathieu@matlux.net>
2014-03-19 19:10:55 +0000

    deployed 0.1.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-chess-engine "0.1.0.3"
+(defproject clj-chess-engine "0.1.0.4"
   :description "Chess Engine written in Clojure"
   :url "http://lambda-zone.com"
   :license {:name "Eclipse Public License"
